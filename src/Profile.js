c6c735a39bbef4e860c4bf05fad5f1f6a80a50fe
JosephSultan <105505623+JosephSultan@users.noreply.github.com>
2023-02-16 15:38:15 +0100

    j

--- src/Profile.js
+++ src/Profile.js
@@ -1,11 +1,30 @@
 import './profile.css'
-import 'thisIsMyCopy.html'
 
 
 function Profile() {
 
   return (
-    <div className="content" dangerouslySetInnerHTML={{__html: thisIsMyCopy}}></div>
+   <div> <link> href='https://fonts.googleapis.com/css?family=Open+Sans:700,600' rel='stylesheet' type='text/css'</link>
+
+    <form method="post" action="index.html">
+    <div class="box">
+    <h1>Dashboard</h1>
+    
+    <input type="email" name="email" value="email" onFocus="field_focus(this, 'email');" onblur="field_blur(this, 'email');" class="email" />
+      
+    <input type="password" name="email" value="email" onFocus="field_focus(this, 'email');" onblur="field_blur(this, 'email');" class="email" />
+      
+    <a href="#"><div class="btn">Sign In</div></a> 
+    <a href="#"><div id="btn2">Sign Up</div></a> 
+      
+    </div> 
+      
+    </form>
+    
+    <p>Forgot your password? <u style="color:#f1c40f;">Click Here!</u></p>
+      
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" type="text/javascript"></script>
+    </div>
   )
 }
 
