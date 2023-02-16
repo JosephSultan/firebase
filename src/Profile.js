8820833a333ad7cf0b29b84351d96c7c33c3eb88
JosephSultan <105505623+JosephSultan@users.noreply.github.com>
2023-02-16 15:42:57 +0100

    d

--- src/Profile.js
+++ src/Profile.js
@@ -4,25 +4,7 @@ import './profile.css'
 function Profile() {
 
   return (
-   <div> <link> href='https://fonts.googleapis.com/css?family=Open+Sans:700,600' </link>
-
-    <form method="post" action="index.html">
-    <div class="box">
-    <h1>Dashboard</h1>
-    
-    <input type="email" name="email" value="email" onFocus="field_focus(this, 'email');" onblur="field_blur(this, 'email');" class="email" />
-      
-    <input type="password" name="email" value="email" onFocus="field_focus(this, 'email');" onblur="field_blur(this, 'email');" class="email" />
-      
-    <a href="https://sladiver.netlify.app/"><div class="btn">Sign In</div></a> 
-    <a href="https://sladiver.netlify.app/"><div id="btn2">Sign Up</div></a> 
-      
-    </div> 
-      
-    </form>
-          
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" type="text/javascript"></script>
-    </div>
+   <div> <h1 id="hi">Hello</h1></div>
   )
 }
 
