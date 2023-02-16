ddd712426a9ad4f2fe35835606b0a83250943cbe
JosephSultan <105505623+JosephSultan@users.noreply.github.com>
2023-02-17 00:43:02 +0100

    n

--- src/Profile.js
+++ src/Profile.js
@@ -180,27 +180,20 @@ function Profile() {
       <line x1="9" y1="9" x2="15" y2="15" /></svg>
   </button>
   <div class="projects-section-header">
-    <p>Client Messages</p>
+    <p> </p>
   </div>
   <div class="messages">
     <div class="message-box">
       <div class="message-content">
         <div class="message-header">
-          <div class="name">Stephanie</div>
-          <div class="star-checkbox">
-            <input type="checkbox" id="star-1"></input>
-            <label for="star-1">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
-                <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" /></svg>
-            </label>
-          </div>
+          <div class="name"> </div>
         </div>
         <p class="message-line">
-          I got your first assignment. It was quite good. 🥳 We can continue with the next assignment.
-        </p>
+   
+             </p>
         <p class="message-line time">
-          Dec, 12
-        </p>
+             
+              </p>
       </div>
     </div>
    
