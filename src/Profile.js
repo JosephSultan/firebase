5f12f08a526f737919e2dc0124686105e62a7a47
JosephSultan <105505623+JosephSultan@users.noreply.github.com>
2023-02-17 00:06:44 +0100

    d

--- src/Profile.js
+++ src/Profile.js
@@ -1,12 +1,8 @@
 import './Profilenew.css';
-import { setupMeko } from './ProfileAssry.js';
 
 
 
 function Profile() {
-    document.addEventListener('DOMContentLoaded', function () {
-      setupMeko();
-    });
 
     return (
 <div class="app-container">
@@ -23,11 +19,7 @@ function Profile() {
           <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path>
         </svg>
       </button>
-      <button class="notification-btn">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell">
-          <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
-          <path d="M13.73 21a2 2 0 0 1-3.46 0" /></svg>
-      </button>
+      
       <button class="profile-btn">
         <span>Best Team</span>
       </button>
@@ -66,16 +58,7 @@ function Profile() {
             <span class="status-type">Simple steps to connect to SLA Dive</span>
           </div>
         </div>
-        <div class="view-actions">
-          <button class="view-btn list-view" title="List View">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-list">
-              <line x1="8" y1="6" x2="21" y2="6" />
-              <line x1="8" y1="12" x2="21" y2="12" />
-              <line x1="8" y1="18" x2="21" y2="18" />
-              <line x1="3" y1="6" x2="3.01" y2="6" />
-              <line x1="3" y1="12" x2="3.01" y2="12" />
-              <line x1="3" y1="18" x2="3.01" y2="18" /></svg>
-          </button>
+     
           <button class="view-btn grid-view active" title="Grid View">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-grid">
               <rect x="3" y="3" width="7" height="7" />
@@ -89,7 +72,7 @@ function Profile() {
         <div class="project-box-wrapper">
           <div class="project-box"  style={{ backgroundColor: '#fee4cb' }}>
             <div class="project-box-header">
-              <span>December 10, 2020</span>
+              <span> </span>
               <div class="more-wrapper">
                 <button class="project-btn-more">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical">
@@ -100,29 +83,27 @@ function Profile() {
           </div>
         </div>
         <div class="project-box-content-header">
-          <p class="box-content-header">Web Designing</p>
-          <p class="box-content-subheader">Prototyping</p>
+          <p class="box-content-header">Sign in with Intercom</p>
+          <p class="box-content-subheader">Use our secure Oauth SignIn</p>
         </div>
         <div class="box-progress-wrapper">
-          <p class="box-progress-header">Progress</p>
+          <p class="box-progress-header"> </p>
           <div class="box-progress-bar">
-            <span class="box-progress"  style={{ width: '60%',backgroundColor: '#ff942e' }}></span>
+            <span class="box-progress"  style={{ width: '100%',backgroundColor: '#ff942e' }}></span>
           </div>
-          <p class="box-progress-percentage">60%</p>
+          <p class="box-progress-percentage"> </p>
         </div>
         <div class="project-box-footer">
           <div class="participants">
-            <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="participant"></img>
-            <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fG1hbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60" alt="participant"></img>
-            <button class="add-participant"  style={{ backgroundColor: '#ff942e' }}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus">
-                <path d="M12 5v14M5 12h14" />
-              </svg>
-            </button>
+        
           </div>
-          <div class="days-left" style={{ backgroundColor: '#ff942e' }}>
-            2 Days Left
-          </div>
+
+          <button class="days-left" style={{ backgroundColor: '#ff942e' }} onClick={() => window.open('https://www.facebook.com/', '_blank')}>
+  Facebook sign in
+</button>
+<div class="days-left" style={{ backgroundColor: '#ff942e' }}>
+  2 Days Left
+</div>
         </div>
       </div>
     </div>
