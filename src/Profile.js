fd8ed24bd62b433ce26bd80510b692cfee0bc673
JosephSultan <105505623+JosephSultan@users.noreply.github.com>
2023-02-17 00:40:16 +0100

    d

--- src/Profile.js
+++ src/Profile.js
@@ -184,7 +184,6 @@ function Profile() {
   </div>
   <div class="messages">
     <div class="message-box">
-      <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="profile"></img>
       <div class="message-content">
         <div class="message-header">
           <div class="name">Stephanie</div>
@@ -204,38 +203,12 @@ function Profile() {
         </p>
       </div>
     </div>
-    <div class="message-box">
-      <img src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="profilex"></img>
-      <div class="message-content">
-        <div class="message-header">
-          <div class="name">Mark</div>
-          <div class="star-checkbox">
-            <input type="checkbox" id="star-2"></input>
-            <label for="star-2">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
-                <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" /></svg>
-            </label>
-          </div>
-        </div>
-        <p class="message-line">
-          Hey, can tell me about progress of project? I'm waiting for your response.
-        </p>
-        <p class="message-line time">
-          Dec, 12
-        </p>
-      </div>
-    </div>
-        </div>
-        <p class="message-line">
-          Awesome! 🤩 I like it. We can schedule a meeting for the next one.
-        </p>
-        <p class="message-line time">
-          Dec, 12
-        </p>
-      </div>
-    </div>
    
+       
 
+  </div>
+  </div>
+  </div>
   </div>
   </div>
   </div>
