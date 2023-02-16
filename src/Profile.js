5826fed04fea8de04a62cd04c30739ff2b7bac2c
JosephSultan <105505623+JosephSultan@users.noreply.github.com>
2023-02-16 19:20:32 +0100

    d

--- src/Profile.js
+++ src/Profile.js
@@ -42,12 +42,7 @@ function Profile() {
           <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
           <polyline points="9 22 9 12 15 12 15 22" /></svg>
       </a>
-      <a href="https://sladiver.netlify.app/" class="app-sidebar-link">
-        <svg class="link-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="feather feather-pie-chart" viewBox="0 0 24 24">
-          <defs />
-          <path d="M21.21 15.89A10 10 0 118 2.83M22 12A10 10 0 0012 2v10z" />
-        </svg>
-      </a>
+    
       <a href="https://sladiver.netlify.app/" class="app-sidebar-link">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar">
           <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
@@ -55,13 +50,7 @@ function Profile() {
           <line x1="8" y1="2" x2="8" y2="6" />
           <line x1="3" y1="10" x2="21" y2="10" /></svg>
       </a>
-      <a href="https://sladiver.netlify.app/" class="app-sidebar-link">
-        <svg class="link-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="feather feather-settings" viewBox="0 0 24 24">
-          <defs />
-          <circle cx="12" cy="12" r="3" />
-          <path d="M19.4 15a1.65 1.65 0 00.33 1.82l.06.06a2 2 0 010 2.83 2 2 0 01-2.83 0l-.06-.06a1.65 1.65 0 00-1.82-.33 1.65 1.65 0 00-1 1.51V21a2 2 0 01-2 2 2 2 0 01-2-2v-.09A1.65 1.65 0 009 19.4a1.65 1.65 0 00-1.82.33l-.06.06a2 2 0 01-2.83 0 2 2 0 010-2.83l.06-.06a1.65 1.65 0 00.33-1.82 1.65 1.65 0 00-1.51-1H3a2 2 0 01-2-2 2 2 0 012-2h.09A1.65 1.65 0 004.6 9a1.65 1.65 0 00-.33-1.82l-.06-.06a2 2 0 010-2.83 2 2 0 012.83 0l.06.06a1.65 1.65 0 001.82.33H9a1.65 1.65 0 001-1.51V3a2 2 0 012-2 2 2 0 012 2v.09a1.65 1.65 0 001 1.51 1.65 1.65 0 001.82-.33l.06-.06a2 2 0 012.83 0 2 2 0 010 2.83l-.06.06a1.65 1.65 0 00-.33 1.82V9a1.65 1.65 0 001.51 1H21a2 2 0 012 2 2 2 0 01-2 2h-.09a1.65 1.65 0 00-1.51 1z" />
-        </svg>
-      </a>
+    
     </div>
     <div class="projects-section">
       <div class="projects-section-header">
@@ -104,7 +93,7 @@ function Profile() {
       </div>
       <div class="project-boxes jsGridView">
         <div class="project-box-wrapper">
-          <div class="project-box" style="background-color: #fee4cb;">
+          <div class="project-box"  style={{ backgroundColor: '#fee4cb' }}>
             <div class="project-box-header">
               <span>December 10, 2020</span>
               <div class="more-wrapper">
@@ -123,19 +112,19 @@ function Profile() {
         <div class="box-progress-wrapper">
           <p class="box-progress-header">Progress</p>
           <div class="box-progress-bar">
-            <span class="box-progress" style="width: 60%; background-color: #ff942e"></span>
+            <span class="box-progress" style={{ width: '60%', backgroundcolor: '#ff942e' }}></span>
           </div>
           <p class="box-progress-percentage">60%</p>
         </div>
         <div class="project-box-footer">
           <div class="participants">
-           <button class="add-participant" style="color: #ff942e;">
+           <button class="add-participant" style={{ color: '#ff942e' }} >
               <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus">
                 <path d="M12 5v14M5 12h14" />
               </svg>
             </button>
           </div>
-          <div class="days-left" style="color: #ff942e;">
+          <div class="days-left" style={{color: '#ff942e'}}>
             2 Days Left
           </div>
         </div>
