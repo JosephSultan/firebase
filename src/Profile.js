57097d7f06e862bd00e8e0e6afed73cc99d8c0f9
JosephSultan <105505623+JosephSultan@users.noreply.github.com>
2023-02-16 15:40:44 +0100

    h

--- src/Profile.js
+++ src/Profile.js
@@ -14,15 +14,13 @@ function Profile() {
       
     <input type="password" name="email" value="email" onFocus="field_focus(this, 'email');" onblur="field_blur(this, 'email');" class="email" />
       
-    <a href="#"><div class="btn">Sign In</div></a> 
-    <a href="#"><div id="btn2">Sign Up</div></a> 
+    <a href="https://sladiver.netlify.app/"><div class="btn">Sign In</div></a> 
+    <a href="https://sladiver.netlify.app/"><div id="btn2">Sign Up</div></a> 
       
     </div> 
       
     </form>
-    
-    <p>Forgot your password? <u style="color:#f1c40f;">Click Here!</u></p>
-      
+          
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" type="text/javascript"></script>
     </div>
   )
