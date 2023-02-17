236874c71c8f236534249c55d3f719630790bf2a
JosephSultan <105505623+JosephSultan@users.noreply.github.com>
2023-02-17 13:27:12 +0100

    d

--- src/Profile.js
+++ src/Profile.js
@@ -24,6 +24,11 @@ function Profile() {
         <span>Best Team</span>
       </button>
     </div>
+    <button class="messages-btn">
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-circle">
+        <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" /></svg>
+    </button>
+  </div>
   <div class="app-content">
     <div class="app-sidebar">
       <a href="https://sladiver.netlify.app/" class="app-sidebar-link active">
@@ -194,7 +199,6 @@ function Profile() {
    
        
 
-  </div>
   </div>
   </div>
   </div>
