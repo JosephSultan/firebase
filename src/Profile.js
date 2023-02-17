28e19ace9175742a6c3634e42d0dc29ad4fb0d2d
JosephSultan <105505623+JosephSultan@users.noreply.github.com>
2023-02-17 13:31:47 +0100

    d

--- src/Profile.js
+++ src/Profile.js
@@ -132,38 +132,10 @@ function Profile() {
        
         </div>
       </div>
-    </div>
-    <div class="project-box-wrapper">
-      <div class="project-box" style={{ backgroundColor: '#e9e7fd'}}>
-        <div class="project-box-header">
-          <span> </span>
-          <div class="more-wrapper">
-            <button class="project-btn-more">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical">
-                <circle cx="12" cy="12" r="1" />
-                <circle cx="12" cy="5" r="1" />
-                <circle cx="12" cy="19" r="1" /></svg>
-            </button>
-          </div>
-        </div>
-        <div class="project-box-content-header">
-          <p class="box-content-header">Add to Slack</p>
-          <p class="box-content-subheader">Secure Oauth2</p>
-        </div>
-        <div class="box-progress-wrapper">
-          <p class="box-progress-header"> </p>
-          <div class="box-progress-bar">
-            <span class="box-progress" style={{width: '100%', backgroundColor: '#4f3ff0'}}></span>
-          </div>
-          <p class="box-progress-percentage"> </p>
-          <div class="project-box-footer">
-        <button class="days-left" style={{ backgroundColor: '#ff942e' }} onClick={() => window.open('https://sladive.tk/slack', '_blank')}>
-            Add to Slack
-            </button>
-       
-        </div>
-      </div>
-    </div>
+
+   
+ 
+
        
 
 
